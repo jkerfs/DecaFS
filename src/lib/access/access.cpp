a34bceab203a0a6f256ce0347242485e8b4b6bba
jkerfs <jkerfs@calpoly.edu>
2015-10-27 13:24:36 -0700

    reenable cleansing of cache

--- src/lib/access/access.cpp
+++ src/lib/access/access.cpp
@@ -3,34 +3,24 @@
 #include <stdio.h>
 #include <string>
 #include <algorithm>
+
 #define CACHE_SIZE 10
 
 
-typedef struct Info {
- void * data;
- int count;
-} Info;
-
 unordered_map<string, char *> cache;
 
+//convert the file_id, stripe_id, and chunk_num into a unique string
 string makeKeyStr(int file_id, int stripe_id, int chunk_num) {
   char temp[30];
   sprintf(temp, "%8d;%8d;%8d", file_id, stripe_id, chunk_num);
   return std::string(temp);
 }
 
-/*void printHead(char *str, int count) {
-  for (int i = 0; i < count; i++) {
-   std::cout << std::hex << (int)str[i];
-  }
-  std::cout << std::endl;
-}*/
-
 ssize_t process_read_chunk (uint32_t request_id, int fd, int file_id,
                             int node_id, int stripe_id, int chunk_num,
                             int offset, void* buf, int count) {
- std::cout << "offset: " << offset << std::endl;
 
+ //compute the key for the given file, stripe and chunk
   string key = makeKeyStr(file_id, stripe_id, chunk_num);
   std::cout << "KEY: " << key << std::endl;
 
@@ -43,7 +33,6 @@ ssize_t process_read_chunk (uint32_t request_id, int fd, int file_id,
 
      read_response_handler(&readResponse);
 
-     /*memcpy(buf, cache[key][offset], count*sizeof(uint8_t));*/
      std::cout << "Retrieved from cache!" << std::endl;
      std::cout << base << std::endl;
 
@@ -58,20 +47,27 @@ ssize_t process_write_chunk (uint32_t request_id, int fd, int file_id,
                              int node_id, int stripe_id, int chunk_num,
                              int offset, void *buf, int count) {
 
-/*
+  // Remove an element from cache if cache is full
   if (cache.size() >= CACHE_SIZE) {
-    std::cout << "REMOVING " << cache.begin()->first << std::endl;
-    //free((void*)((cache.begin()->first).data));
+    std::cout << "REMOVING element from Cache" << std::endl;
+    free(cache.begin()->second);
     cache.erase(cache.begin());
-  }*/
-  string key = makeKeyStr(file_id, stripe_id, chunk_num);
-  if (!cache.count(key)) {
+  }
 
+  //compute the key for the given file, stripe and chunk
+  string key = makeKeyStr(file_id, stripe_id, chunk_num);
+
+  // allocate memory to store chunk
+  if (!cache.count(key)) {
     cache[key] = (char*) malloc(MAX_CHUNK);
   }
 
+  //copy chunk to cache at specific key and offset
   char* base = &((cache[key])[offset]);
   memcpy(base, buf, count);
+
+  WriteChunkResponse writeResponse(request_id, fd, file_id, stripe_id, chunk_num, offset, count);
+  write_response_handler(&writeResponse);
 
   return network_write_chunk (request_id, fd, file_id, node_id, stripe_id,
                               chunk_num, offset, buf, count);
@@ -80,13 +76,15 @@ ssize_t process_write_chunk (uint32_t request_id, int fd, int file_id,
 
 ssize_t process_delete_chunk (uint32_t request_id, int file_id, int node_id,
                               int stripe_id, int chunk_num) {
-  /*std::string key = makeKeyStr(file_id, stripe_id, chunk_num);
-  auto it = cache.find(key);
-  if (it != cache.end()) {
-    //free((it->first).data);
-    cache.erase(it);
+
+  //compute the key for the given file, stripe and chunk
+  string key = makeKeyStr(file_id, stripe_id, chunk_num);
+
+  //free memory and remove chunk from cache if it exists
+  if (cache.count(key)) {
+    free(cache.find(key)->second);
+    cache.erase(key);
   }
-*/
-  return network_delete_chunk (request_id, file_id, node_id, stripe_id,
-                               chunk_num);
+
+  return network_delete_chunk (request_id, file_id, node_id, stripe_id, chunk_num);
 }
