f36b4c0c98b8c9bb863872066e74367d69cb6647
jkerfs <jkerfs@calpoly.edu>
2015-10-27 10:35:22 -0700

    clean slate

--- src/lib/access/access.cpp
+++ src/lib/access/access.cpp
@@ -30,7 +30,7 @@ ssize_t process_read_chunk (uint32_t request_id, int fd, int file_id,
                             int offset, void* buf, int count) {
   string key = makeKeyStr(file_id, chunk_num);
   std::cout << "KEY: " << key << std::endl;
-  if (cache.count(key) > 0) {
+  /*if (cache.count(key) > 0) {
     Info res = cache[key];
     std::cout << "FOUND " << std::endl;
     if (res.count >= count) {
@@ -39,7 +39,7 @@ ssize_t process_read_chunk (uint32_t request_id, int fd, int file_id,
      std::cout << "Retrieved from cache!" << std::endl;
      return count;
     }
-  }
+  }*/
 
   return network_read_chunk (request_id, fd, file_id, node_id, stripe_id,
                              chunk_num, offset, count);
@@ -50,7 +50,7 @@ ssize_t process_write_chunk (uint32_t request_id, int fd, int file_id,
                              int offset, void *buf, int count) {
 
 
-  if (cache.size() >= CACHE_SIZE) {
+  /*if (cache.size() >= CACHE_SIZE) {
     std::cout << "REMOVING " << cache.begin()->first << std::endl;
     //free((void*)((cache.begin()->first).data));
     cache.erase(cache.begin());
@@ -59,7 +59,7 @@ ssize_t process_write_chunk (uint32_t request_id, int fd, int file_id,
   memcpy(temp, buf, count * sizeof(uint8_t));
 
   cache[makeKeyStr(file_id, chunk_num)] = Info{temp, count};
-
+*/
   return network_write_chunk (request_id, fd, file_id, node_id, stripe_id,
                               chunk_num, offset, buf, count);
 }
@@ -67,13 +67,13 @@ ssize_t process_write_chunk (uint32_t request_id, int fd, int file_id,
 
 ssize_t process_delete_chunk (uint32_t request_id, int file_id, int node_id,
                               int stripe_id, int chunk_num) {
-  std::string key = makeKeyStr(file_id, chunk_num);
+  /*std::string key = makeKeyStr(file_id, chunk_num);
   auto it = cache.find(key);
   if (it != cache.end()) {
     //free((it->first).data);
     cache.erase(it);
   }
-
+*/
   return network_delete_chunk (request_id, file_id, node_id, stripe_id,
                                chunk_num);
 }
