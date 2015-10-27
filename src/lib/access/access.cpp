5a6650d50e44cd74d0c3fec1025673f6d8a18f53
jkerfs <jkerfs@calpoly.edu>
2015-10-27 10:56:50 -0700

    include cache again

--- src/lib/access/access.cpp
+++ src/lib/access/access.cpp
@@ -4,17 +4,18 @@
 #include <string>
 #include <algorithm>
 #define CACHE_SIZE 10
-/*
+
+
 typedef struct Info {
  void * data;
  int count;
 } Info;
-*/
-//unordered_map<string, Info> cache;
+
+unordered_map<string, Info> cache;
 
 string makeKeyStr(int file_id, int chunk_num) {
-  char temp[15];
-  sprintf(temp, "%5d;%9d", file_id, chunk_num);
+  char temp[20];
+  sprintf(temp, "%8d;%8d", file_id, chunk_num);
   return std::string(temp);
 }
 
@@ -28,9 +29,9 @@ string makeKeyStr(int file_id, int chunk_num) {
 ssize_t process_read_chunk (uint32_t request_id, int fd, int file_id,
                             int node_id, int stripe_id, int chunk_num,
                             int offset, void* buf, int count) {
-  //string key = makeKeyStr(file_id, chunk_num);
-  //std::cout << "KEY: " << key << std::endl;
-  /*if (cache.count(key) > 0) {
+  string key = makeKeyStr(file_id, chunk_num);
+  std::cout << "KEY: " << key << std::endl;
+  if (cache.count(key) > 0) {
     Info res = cache[key];
     std::cout << "FOUND " << std::endl;
     if (res.count >= count) {
@@ -39,7 +40,7 @@ ssize_t process_read_chunk (uint32_t request_id, int fd, int file_id,
      std::cout << "Retrieved from cache!" << std::endl;
      return count;
     }
-  }*/
+  }
 
   return network_read_chunk (request_id, fd, file_id, node_id, stripe_id,
                              chunk_num, offset, count);
@@ -49,17 +50,17 @@ ssize_t process_write_chunk (uint32_t request_id, int fd, int file_id,
                              int node_id, int stripe_id, int chunk_num,
                              int offset, void *buf, int count) {
 
-
-  /*if (cache.size() >= CACHE_SIZE) {
+/*
+  if (cache.size() >= CACHE_SIZE) {
     std::cout << "REMOVING " << cache.begin()->first << std::endl;
     //free((void*)((cache.begin()->first).data));
     cache.erase(cache.begin());
-  }
+  }*/
   void * temp = calloc(sizeof(uint8_t), count);
   memcpy(temp, buf, count * sizeof(uint8_t));
 
   cache[makeKeyStr(file_id, chunk_num)] = Info{temp, count};
-*/
+
   return network_write_chunk (request_id, fd, file_id, node_id, stripe_id,
                               chunk_num, offset, buf, count);
 }
